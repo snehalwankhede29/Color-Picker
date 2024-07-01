917cc1bd048519f023689e581770f51993a6bc0f
Snehal Wankhede <128214459+snehalwankhede29@users.noreply.github.com>
2024-07-01 22:40:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let update = document.getElementById("Background");
+let para1 = document.getElementById("para");
+
+function btn1(){
+    update.style.background="#ff1950";
+    para1.textContent="Background Color : #ff1950";
+}
+function btn2(){
+    update.style.background="#0000b2";
+    para1.textContent="Background Color : #0000b2";
+}
+function btn3(){
+    update.style.background="#f1c232";
+    para1.textContent="Background Color : #f1c232";
+}
+function btn4(){
+    update.style.background="#b2b2ff";
+    para1.textContent="Background Color : #b2b2ff";
+}
